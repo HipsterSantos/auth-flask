ac37377e0b8b3beaa837ed7b00d8384b87594563
HipsterSantos <santoscampos269@gmail.com>
2024-05-01 04:08:19 +0100

    update model

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 from extension import db,jwt
 from werkzeug.security import generate_password_hash,check_password_hash
-class User:
+class User(db.models):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64),unique=True,nullable=False)
     password_hash = db.Column(db.String(128),nullable=False)
